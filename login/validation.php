f61202e6c8b985a9f40d904e6e8537f6b1010a41
alvdev <soy@alv.dev>
2021-10-22 12:19:06 +0200

    Añadido md5 a la verificación de acceso

--- login/validation.php
+++ login/validation.php
@@ -7,11 +7,13 @@
 
 require_once 'sessions.php';
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     foreach ($users as $user) {
         if (
             $_SESSION['usu_nombre'] === $user['usu_nombre'] &&
-            $_SESSION['usu_password'] === $user['usu_password']
+            md5($_SESSION['usu_password']) === $user['usu_password']
         ) {
             header('Location: login/success.php');
         }
