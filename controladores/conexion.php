4b71e78a09c5e9a7598b1c9118299205a5fb4969
naxo <ijiruiz@gmail.com>
2021-10-22 09:33:27 +0200

    21102021

--- controladores/conexion.php
+++ controladores/conexion.php
@@ -11,8 +11,8 @@ class Conexion
 
         try {
             // Crear una conexión instanciando un objeto PDO y seteándolo
-            $PDO = new PDO(SERVERBD, USERBD, PASSBD);
-            //$PDO = new PDO('mysql:host=localhost; dbname=bd_proyectos', 'root', "");
+            //$PDO = new PDO(SERVERBD, USERBD, PASSBD);
+            $PDO = new PDO('mysql:host=localhost; dbname=bd_proyectos', 'root', "");
             $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $PDO->exec("SET CHARACTER SET utf8");
 
