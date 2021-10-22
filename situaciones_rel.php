82bc92383372d13230eeb12b9dca4d5b959a9778
naxo <ijiruiz@gmail.com>
2021-10-22 13:30:50 +0200

    221020211330

--- situaciones_rel.php
+++ situaciones_rel.php
@@ -1,15 +1,53 @@
 <?php
-echo"SITUACIONES REL";
-    require_once("controladores/controlador.php");
-    $sqlp="SELECT p.proy_nombre,s.sit_nombre FROM proyectos p,situaciones s WHERE p.proy_sit_id=s.sit_id";
-    $sqlt="SELECT t.tar_nombre,s.sit_nombre FROM tareas t,situaciones s WHERE t.tar_sit_id=s.sit_id";
-    $sqla="SELECT a.acc_nombre,s.sit_nombre FROM acciones a,situaciones s WHERE a.acc_sit_id=s.sit_id";
-    echo"<br>PROYECTOS<br>";
-    var_dump( ($proyectos=controlador::select($sqlp)));
-    echo"<br>TAREAS<br>";
-    var_dump( ($tareas=controlador::select($sqlt)));
-    echo"<br>ACCIONES<br>";
-    var_dump( ($acciones=controlador::select($sqla)));
-    
+
+require_once("controladores/controlador.php");
+
+$txt = "";
+$sql = "SELECT * FROM situaciones";
+$situaciones = json_decode(controlador::select($sql), true);
+var_dump($situaciones);
 
 ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relacion de Situaciones</title>
+    <script>
+        function fInicio() {
+
+        }
+    </script>
+</head>
+
+<body onload="fInicio()">
+    <div class="div_rel_situaciones">
+        <form action="situaciones_frm.php" method="get">
+        <table class="tb_situaciones" border='1'>
+            <tr>
+                <th>ID </th>
+                <th>NOMBRE</th>
+                <th colspan='2'>ACCIONES</th>
+            </tr>
+            <?php
+            $txt = "";
+            foreach ($situaciones as $key => $value) {
+                //$txt="<tr><td>";
+                $txt .= "<tr><td>" . $value['sit_id'] . "</td><td>" . $value['sit_nombre'] . "</td>";
+                $txt .= "<td><input type='submit' value='MODIFICAR'><input type='submit' value='ELIMINAR'></tr>";
+            }
+            echo $txt;
+            ?>
+
+        </table>
+        <div class="div_insertar">
+            <input type="submit" value='AÑADIR SITUACION'>
+        </div>
+        </form>
+    </div>
+</body>
+
+</html>
