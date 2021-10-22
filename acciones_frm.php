fd98d9e4bf134493994b60709b2753f7ca55f507
adrian <adrianrodriguezba@gmail.com>
2021-10-22 11:19:53 +0200

    master

--- acciones_frm.php
+++ acciones_frm.php
@@ -5,7 +5,7 @@ if (isset($_POST['modificar'])) {
     $accion_id = $_POST['modificar'];
     $sql = "SELECT * FROM acciones WHERE acc_id = $accion_id";
     $accion = json_decode(controlador::select($sql), true);
-    $t = "<form action='' method='POST'>";
+    $t = "<form action='acciones_rel.php' method='POST'>";
     for ($i = 0; $i < count($accion); $i++) {
         $nombre = $accion[$i]["acc_nombre"];
         $f_r_ini = $accion[$i]["acc_fr_inicio"];
@@ -17,6 +17,7 @@ if (isset($_POST['modificar'])) {
         $situaciones_id = $accion[$i]["acc_sit_id"];
         $proyecto_id = $accion[$i]["acc_proy_id"];
         $observaciones = $accion[$i]["acc_obs"];
+        $t .= "<label>ID<input type='number' name='id' id='id' value='$accion_id' disabled></label>";
         $t .= "<label>Nombre<input type='text' name='accion' id='accion' value='$nombre'></label>";
         $t .= "<label>Fecha real inicio<input type='date' name='f_r_ini' id='f_r_ini' value='$f_r_ini'></label>";
         $t .= "<label>Fecha real fin<input type='date' name='f_r_fin' id='f_r_fin' value='$f_r_fin'></label>";
@@ -24,29 +25,13 @@ if (isset($_POST['modificar'])) {
         $t .= "<label>Fecha teórica fin<input type='date' name='f_t_fin' id='f_t_fin' value='$f_t_fin'></label>";
         $t .= "<label>ID del usuario<input type='number' name='usu_id' id='usu_id' value='$usu_id' readonly></label>";
         $t .= "<label>Duración<input type='number' name='duracion' id='duracion' value='$duracion'></label>";
-        $t .= "<label>ID situaciones<input type='number' name='situaciones_id' id='situaciones_id' value='$situaciones_id'></label>";
-        $t .= "<label>ID del proyecto<input type='text' name='proyecto_id' id='proyecto_id' value='$proyecto_id'></label>";
+        $t .= "<label>ID situaciones<input type='number' name='id_sit' id='situaciones_id' value='$situaciones_id'></label>";
+        $t .= "<label>ID del proyecto<input type='text' name='id_proy' id='proyecto_id' value='$proyecto_id'></label>";
         $t .= "<label>Observaciones<textarea name='obs' id='obs' cols='30' rows='10'>$observaciones</textarea></label>";
         $t .= "<input type='hidden' name='modificado'>";
         $t .= "<input type='submit' value='Modificar'>";
     }
     $t .= "</form>";
-
-    if (isset($_POST['modificado'])) {
-        $nombre_v = $_POST['accion'];
-        $f_r_ini_v = $_POST['f_r_ini'];
-        $f_r_fin_v = $_POST['f_r_fin'];
-        $f_t_ini_v = $_POST['f_t_ini'];
-        $f_t_fin_v = $_POST['f_t_fin'];
-        $usu_id_v = $_POST['usu_id'];
-        $duracion_v = $_POST['duracion'];
-        $situaciones_id_v = $_POST['situaciones_id'];
-        $proyecto_id_v = $_POST['proyecto_id'];
-        $observaciones_v = $_POST['obs'];
-        $sql = "UPDATE acciones WHERE acc_id = $accion_id SET acc_nombre = '$nombre_v', acc_fr_inicio = $f_r_ini_v, acc_fr_fin = $f_r_fin_v, acc_ft_inicio = $f_t_ini_v, acc_ft_fin = $f_t_fin_v, acc_usu_id = $usu_id_v, acc_duracion = $duracion_v, acc_sit_id = $situaciones_id_v, acc_proy_id = $proyecto_id_v, acc_obs = '$observaciones_v')";
-        $afectados = controlador::update($sql);
-        header("Location:acciones_rel.php");
-    }
 }
 ?>
 
@@ -65,17 +50,17 @@ if (isset($_POST['modificar'])) {
 
     if (isset($_POST['agregar'])) {
     ?>
-        <form action="" method="POST">
+        <form action="acciones_rel.php" method="POST">
             <label for="accion">Acción</label>
             <input type="text" name="accion" id="accion">
             <label for="fecha_real_inicio">Fecha real inicio</label>
-            <input type="date" name="fecha_real_inicio" id="fecha_real_inicio">
+            <input type="date" name="f_r_ini" id="fecha_real_inicio">
             <label for="fecha_real_fin">Fecha real fin</label>
-            <input type="date" name="fecha_real_fin" id="fecha_real_fin">
+            <input type="date" name="f_r_fin" id="fecha_real_fin">
             <label for="fecha_teorica_inicio">Fecha teórica inicio</label>
-            <input type="date" name="fecha_teorica_inicio" id="fecha_teorica_inicio">
+            <input type="date" name="f_t_ini" id="fecha_teorica_inicio">
             <label for="fecha_teorica_fin">Fecha teórica fin</label>
-            <input type="date" name="fecha_teorica_fin" id="fecha_teorica_fin">
+            <input type="date" name="f_t_fin" id="fecha_teorica_fin">
             <label for="usu_id">ID usuario</label>
             <input type="number" name="usu_id" id="usu_id">
             <label for="duracion">Duración</label>
@@ -89,22 +74,6 @@ if (isset($_POST['modificar'])) {
             <input type="submit" value="Agregar">
         </form>
     <?php
-        if (isset($_POST['agregado'])) {
-
-            $nombre_v = $_POST['accion'];
-            $f_r_ini_v = $_POST['fecha_real_inicio'];
-            $f_r_fin_v = $_POST['fecha_real_fin'];
-            $f_t_ini_v = $_POST['fecha_teorica_inicio'];
-            $f_t_fin_v = $_POST['fecha_teorica_fin'];
-            $usu_id_v = $_POST['usu_id'];
-            $duracion_v = $_POST['duracion'];
-            $situaciones_id_v = $_POST['id_sit'];
-            $proyecto_id_v = $_POST['id_proy'];
-            $observaciones_v = $_POST['obs'];
-            $sql = "INSERT INTO acciones VALUES (null, '$nombre_v', $f_r_ini_v, $f_r_fin_v, $f_t_ini_v, $f_t_fin_v, $usu_id_v, $duracion_v, $situaciones_id_v, $proyecto_id_v, '$observaciones_v')";
-            $id_agregado = controlador::insert($sql);
-            header("Location:acciones_rel.php");
-        }
     }
     ?>
 </body>
--- acciones_rel.php
+++ acciones_rel.php
@@ -1,34 +1,44 @@
 <?php
 require_once("controladores/controlador.php");
-if (isset ($_POST['borrar'])) {
+if (isset($_POST['borrar'])) {
     $id_acc = $_POST['borrar'];
     $sql = "DELETE FROM acciones WHERE acc_id = $id_acc";
     controlador::delete($sql);
 }
-// if (isset($_POST['agregado'])) {
-//     $nombre_v = $_POST['accion'];
-//     $f_r_ini_v = $_POST['f_r_ini'];
-//     $f_r_fin_v = $_POST['f_r_fin'];
-//     $f_t_ini_v = $_POST['f_t_ini'];
-//     $f_t_fin_v = $_POST['f_t_fin'];
-//     $usu_id_v = $_POST['usu_id'];
-//     $duracion_v = $_POST['duracion'];
-//     $situaciones_id_v = $_POST['situaciones_id'];
-//     $proyecto_id_v = $_POST['proyecto_id'];
-//     $observaciones_v = $_POST['obs'];
-//     $sql = "INSERT INTO acciones VALUES (null, '$nombre_v', '$f_r_ini_v', '$f_r_fin_v', '$f_t_ini_v', '$f_t_fin_v', '$usu_id_v', '$duracion_v', '$situaciones_id_v', '$proyecto_id_v', '$observaciones_v')";
-//     echo $sql;
-    // $id_agregado = controlador::insert($sql);
-    
-// }
+if (isset($_POST['agregado']) || isset($_POST['modificado'])) {
+    $nombre_v = $_POST['accion'];
+    $f_r_ini_v = $_POST['f_r_ini'];
+    $f_r_fin_v = $_POST['f_r_fin'];
+    $f_t_ini_v = $_POST['f_t_ini'];
+    $f_t_fin_v = $_POST['f_t_fin'];
+    $usu_id_v = $_POST['usu_id'];
+    $duracion_v = $_POST['duracion'];
+    $situaciones_id_v = $_POST['id_sit'];
+    $proyecto_id_v = $_POST['id_proy'];
+    $observaciones_v = $_POST['obs'];
+    if (isset($_POST['agregado'])) {
+        $sql = "INSERT INTO acciones VALUES (null, '$nombre_v', '$f_r_ini_v', '$f_r_fin_v', '$f_t_ini_v', '$f_t_fin_v', $usu_id_v, $duracion_v, $situaciones_id_v, $proyecto_id_v, '$observaciones_v')";
+        $id_agregado = controlador::insert($sql);
+        echo "Insertado con el sql=" . $sql;
+    }
+    if (isset($_POST['modificado'])) {
+        $id_V = $_POST['id'];
+
+        $sql = "UPDATE acciones WHERE acc_id = $accion_id SET acc_nombre = '$nombre_v', acc_fr_inicio = $f_r_ini_v, acc_fr_fin = $f_r_fin_v, acc_ft_inicio = $f_t_ini_v, acc_ft_fin = $f_t_fin_v, acc_usu_id = $usu_id_v, acc_duracion = $duracion_v, acc_sit_id = $situaciones_id_v, acc_proy_id = $proyecto_id_v, acc_obs = '$observaciones_v')";
+        $afectados = controlador::update($sql);
+        echo "Modificado con el sql=" . $sql;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acciones</title>
 </head>
+
 <body>
     <div id="datos">
         <?php
@@ -37,7 +47,7 @@ if (isset ($_POST['borrar'])) {
         $sql = "SELECT * FROM acciones ORDER BY acc_nombre";
         $lista = json_decode(controlador::select($sql), true);
         $t = "<table><tr><th>ID</th><th>ACCIÓN</th><th>FECHA REAL INICIO</th><th>FECHA REAL FIN</th><th>FECHA TEÓRICA INICIO</th><th>FECHA TEÓRICA FIN</th><th>ID USUARIO</th><th>DURACIÓN</th><th>ID SITUACIÓN</th><th>ID PROYECTO</th><th>OBSERVACIONES</th><th>ACCIONES</th></tr>";
-        for ($i = 0; $i<count($lista); $i++) {
+        for ($i = 0; $i < count($lista); $i++) {
             $t .= "<tr>";
             $t .= "<td>" . $lista[$i]["acc_id"] . "</td>";
             $t .= "<td>" . $lista[$i]["acc_nombre"] . "</td>";
@@ -53,10 +63,10 @@ if (isset ($_POST['borrar'])) {
             $t .= "<td>";
             $accion_id = $lista[$i]["acc_id"];
             $t .= "<form action='acciones_frm.php' method='POST'>" .
-                        "<input type='hidden' name='modificar' value='$accion_id'><input type='submit' value='Modificar'>
-                    </form>" . 
-                    "<form action='' method='POST'>" .
-                        "<input type='hidden' name='borrar' value='$accion_id'><input type='submit' value='Borrar'>
+                "<input type='hidden' name='modificar' value='$accion_id'><input type='submit' value='Modificar'>
+                    </form>" .
+                "<form action='' method='POST'>" .
+                "<input type='hidden' name='borrar' value='$accion_id'><input type='submit' value='Borrar'>
                     </form>";
             $t .= "</td>";
             // if ($lista[$i]["acc_usu_id"] == $_SESSION['usuario']['usu_id']) {
@@ -72,15 +82,16 @@ if (isset ($_POST['borrar'])) {
             // }else {
             //     $t .= "<td></td>";
             // }
-            
+
             $t .= "</tr>";
         }
         $t .= "<form action='acciones_frm.php' method='POST'>" .
-                        "<input type='hidden' name='agregar'><input type='submit' value='Agregar'>
+            "<input type='hidden' name='agregar'><input type='submit' value='Agregar'>
                     </form>";
         $t .= "</table>";
         echo $t;
         ?>
     </div>
 </body>
+
 </html>
