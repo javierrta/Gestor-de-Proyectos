5d4c1d41974cf4353933f3f3c162228838bd4c8b
naxo <ijiruiz@gmail.com>
2021-10-25 13:58:57 +0200

    251020211400

--- situaciones_frm.php
+++ situaciones_frm.php
@@ -1,12 +1,12 @@
 
 <div id="div_frm_situaciones" class="div_frm_situaciones" >
     <form action="fAñadir()" method="get">
-        <label id="etiq_frm_situaciones" for="situacion">Crear nueva Situacion</label>
-        <input type="text" name="situacion" id="situacion"><br>
+        <p><label id="etiq_frm_situaciones" for="situacion">Crear nueva Situacion</label></p>
+        <p><input type="text" name="situacion" id="situacion"></p><br>
         <p id="frm_modificar_situaciones">
-        <label id="etiq_frm_situaciones2" class="frm_modificar_situaciones" for="situacion2">Nuevo nombre situacion </label>
-        <input type="text" name="situacion2" id="situacion2" class="frm_modificar_situaciones"></p>
-        <input type="button" id="multivalor" value="Boton Multivalor">
-        <input type="button" id="cancelar" value="Cancelar" onclick="fCancelarFrmSituacion()">
+        <label id="etiq_frm_situaciones2" class="frm_modificar_situaciones" for="situacion2">Nuevo nombre situacion </label></p>
+        <p><input type="text" name="situacion2" id="situacion2" class="frm_modificar_situaciones"></p>
+        <p><input type="button" id="multivalor" value="Boton Multivalor"></p>
+        <p><input type="button" id="cancelar" value="Cancelar" onclick="fCancelarFrmSituacion()"></p>
     </form>
 </div>
